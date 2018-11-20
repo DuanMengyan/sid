81a6eb20afa6276f1b3a462f898668aadb85f954
shanqinwang <wangshanqin@kekehome-macbook.local>
2018-11-20 23:36:05 +0800

    开篇
    项目开始，首先添加一个入口类：SIDownloader

---
+++ sidownloader.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#  -*- coding: UTF-8 -*-
+
+# Author:
+
+"""
+在这个文件中定义主要的入口，启动下载过程
+
+1. 给卫星服务网站地址发送搜索数据的参数，包括地理范围、时间段和云量；
+2. 解析检索结果，生成下载任务；
+3. 启动和维护下载任务；
+4. 将下载的文件存放到指定位置。
+"""
+
+
+class SIDownloader:
+    def __init(self):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def restart(self):
+        pass
+
+
+if __name__ == '__main__':
+    sis = SIDownloader()
+    sis.start()
+
